f5eeb999b5fb7f2a75f7970d0b2707898cd96ea8
kirimaks <kirimaks@yahoo.com>
2016-06-05 20:34:15 +0300

    iface

--- back-end/routes.py
+++ back-end/routes.py
@@ -1,13 +1,23 @@
 from flask import Flask, Response
-app = Flask(__name__)
-
+from flask import render_template
+from flask.ext.mysql import MySQL
 from orgs_search import OrgsSearch
 import json
+
+app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'parsed_data'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!!!"
+    return render_template("hello.html")
 
 
 @app.route("/search/<int:search_id>")
@@ -19,6 +29,31 @@ def search_by_id(search_id):
                     status=200,
                     mimetype="application/json")
     return resp
+
+
+@app.route("/get_id/<name>")
+def get_id(name):
+    name = name.split(" ")
+
+    if not name[0] or not name[1]:
+        return "0"
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    query = """
+        SELECT search_id
+        FROM search
+            WHERE search_string LIKE '%{name}+{surname}%' LIMIT 1
+    """
+
+    cursor.execute(query.format(name=name[0], surname=name[1]))
+    user_id = cursor.fetchone()
+
+    if user_id:
+        return str(user_id[0])
+    else:
+        return "0"
 
 
 if __name__ == "__main__":
