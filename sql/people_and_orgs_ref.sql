afef20fefc90f528163b12b804f98e89bf34338e
kirimaks <kirimaks@yahoo.com>
2016-06-10 08:31:47 +0300

    Reference updater test.

--- sql/people_and_orgs_ref.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE people_and_orgs_ref(
-    ref_id      INTEGER PRIMARY KEY AUTO_INCREMENT,
-    person_id   INTEGER NOT NULL,
-    org_id      INTEGER NOT NULL,
-    relevance   INTEGER NOT NULL,
-    CONSTRAINT pers_org_rel_uc_ref UNIQUE(person_id, org_id, relevance)
-);
